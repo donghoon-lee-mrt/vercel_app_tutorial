0548f14b6c7a6fc21b7f1c907a656911f5504ffe
donghoon lee <donghoon.lee@myrealtrip.com>
2025-10-01 14:35:09 +0900

    한글 텍스트로 변경

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello, Welcome!</h1>
+        <h1>안녕하세요, 환영합니다!</h1>
         <p>
-          This is a simple React.js application. (modified version)
+          이것은 간단한 React.js 애플리케이션입니다. (수정된 버전)
           <br />
-          It is built on Node.js and bundled using Webpack.
+          Node.js로 구축되었으며 Webpack을 사용하여 번들링되었습니다.
         </p>
         <div className="welcome-message">
-          <p>Welcome! This page is built with React components.</p>
+          <p>환영합니다! 이 페이지는 React 컴포넌트로 구축되었습니다.</p>
         </div>
       </header>
     </div>
